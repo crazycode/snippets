eab6877f26d9eb0618ee5aada4e5e4cc137f0fb1
crazycode <crazycode@gmail.com>
2009-07-16 12:47:51 +0800

    fix elisp condition.

--- ruby-mode/shoulda.sh
+++ ruby-mode/shoulda.sh
@@ -1,7 +1,7 @@
 #name : should "..." do ... end
 #key : sh
 #group : rails.shoulda
-#condition : (or rails/unit-test? rails/functional-test?)
+#condition : (or (rails/unit-test?) (rails/functional-test?))
 # --
 should "${description}" do
   $0
